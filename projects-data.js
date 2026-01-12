aeec3d2b901e421a32535dd6422c461598e66afe
De-TechLab <decerrickrizqullohmumtaz@gmail.com>
2026-01-12 16:54:36 +0700

    Add files via upload

---
+++ projects-data.js
@@ -0,0 +1,32 @@
+// projects-data.js
+// SIMPAN file ini di folder yang sama dengan index.html
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Website E-BookVerse",
+    description: "Platform digital untuk membeli buku secara online.",
+    image: "assets/bookverse.png",
+    link: "https://decerrick-sarjana-tidur.github.io/e-book-verse/",
+    tags: ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    id: 2,
+    title: "Website Resep Makanan",
+    description: "Memudahkan orang mencari resep makanan nikmat.",
+    image: "assets/resep.png",
+    link: "https://decerrick-sarjana-tidur.github.io/De-TechLab/",
+    tags: ["HTML", "CSS", "Bootstrap"]
+  },
+  {
+    id: 3,
+    title: "Website Portofolio",
+    description: "Untuk personal branding agar bisa mudah dilihat client.",
+    image: "assets/portfolio.png",
+    link: "https://decerrick-sarjana-tidur.github.io/De-TechLab/",
+    tags: ["HTML", "CSS", "JavaScript"]
+  }
+];
+
+// Untuk browser
+window.projectsData = projectsData;
